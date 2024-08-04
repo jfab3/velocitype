9a06d6e6ace93853d83f9b57e7465ccb7a47bc92
Jonathan Fabry <jonathandfabry@gmail.com>
2024-08-04 17:27:22 -0400

    uses VITE_PROXY_TARGET locally as well

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,19 +1,25 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import { resolve } from 'path'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  build: {
-    outDir: resolve(__dirname, '../backend/build')
-  },
-  server: {
-    proxy: {
-      '/api': {
-        target: process.env.VITE_PROXY_TARGET || 'http://localhost:8000',
-        changeOrigin: true,
-        secure: false
+export default defineConfig(({ mode }) => {
+  // Load env file based on `mode` in the current working directory.
+  // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
+  const env = loadEnv(mode, resolve(__dirname, '../backend'), '');
+
+  return {
+    plugins: [react()],
+    build: {
+      outDir: resolve(__dirname, '../backend/build')
+    },
+    server: {
+      proxy: {
+        '/api': {
+          target: env.VITE_PROXY_TARGET,
+          changeOrigin: true,
+          secure: false
+        }
       }
     }
   }
