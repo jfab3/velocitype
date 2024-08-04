4d30dd952c43151eb3aced29de7328c66f735694
Jonathan Fabry <jonathandfabry@gmail.com>
2024-08-03 23:30:11 -0400

    adds MONGODB_CONNECTION_STRING env variable

--- backend/src/db.js
+++ backend/src/db.js
@@ -3,10 +3,7 @@ import { MongoClient } from 'mongodb';
 let db;
 
 async function connectToDB(callback) {
-    let client;
-    if (process.env.NODE_ENV === 'testing') {
-        client = new MongoClient('mongodb://127.0.0.1:27017');
-    }
+    const client = new MongoClient(process.env.MONGODB_CONNECTION_STRING);
     await client.connect();
     db = client.db('velocitype-db');
     callback();
